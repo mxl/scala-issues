c0199bfef1077465509e0c233adc4d32867d2ab9
Michael Ledin <m.ledin@appheads.ru>
2017-01-18 16:16:28 +0300

    Add code.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,23 @@
 object Main extends App {
+  import org.json4s._
+  import org.json4s.native.JsonMethods._
+
+  val json = """{ "key": {"a": true, "b": 123}, "value": [1,2,3,4] }"""
+
+  case class Key(a: Boolean, b: Int)
+  case class Event(key: Key, value: JValue)
+
+  case class EventKey(key: Key)
+  case class EventValue(value: Seq[Int])
+
+  implicit val formats = DefaultFormats
+
+  val parsedJson = parse(json)
+
+  val event = parsedJson.extract[Event]
+  val value = event.value.extract[Seq[Int]]
+
+  val eventKey = parsedJson.extract[EventKey]
+  val eventValue = parsedJson.extract[EventValue]
+
 }

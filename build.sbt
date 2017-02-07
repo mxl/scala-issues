bca68ced3fe8c172a3a2b417ef7d40122b676efb
Michael Ledin <m.ledin@appheads.ru>
2017-02-07 11:38:59 +0300

    Example.

--- build.sbt
+++ build.sbt
@@ -41,6 +41,7 @@ lazy val root = project.in(file("."))
   .settings(commonSettings: _*)
   .settings(
     libraryDependencies ++= Seq(
+      "io.getquill" %% "quill" % "1.1.0"
     )
   )
   .dependsOn(macrosModule)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,2 +1,23 @@
+import io.getquill._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 object Main extends App {
+  val db = new MysqlAsyncContext[SnakeCase]("")
+
+  import db._
+
+  case class Session(sid: Int, uid: Int) extends Embedded
+
+  case class TokenStorage(session: Session, selector: String)
+
+  implicit val tokenStorageSchemaMeta =
+    schemaMeta[TokenStorage]("token", _.session.sid -> "sessionId", _.session.uid -> "userId")
+
+  def findBySelector(selector: String): Future[Option[TokenStorage]] =
+    db.run {
+      query[TokenStorage].filter(_.selector == lift(selector))
+    }
+      .map(_.headOption)
 }

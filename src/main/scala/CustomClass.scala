a62b8d4010b17df2801f80c991ab04a195125782
Michael Ledin <m.ledin@appheads.ru>
2017-07-16 15:07:34 +0300

    Simplify.

--- src/main/scala/CustomClass.scala
+++
@@ -1,5 +0,0 @@
-class CustomClass[T] private (val value: T) extends AnyVal
-
-object CustomClass {
-  def apply[T](value: T) = new CustomClass[T](value)
-}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,17 @@
 import io.getquill.{Literal, MirrorContext, MirrorIdiom}
 
 object Main extends App {
-  type CustomString = CustomClass[String]
+  class CustomString private (val value: String) extends AnyVal
+
+  object CustomString {
+    def apply(value: String) = new CustomString(value)
+  }
+
   val ctx = new MirrorContext[MirrorIdiom, Literal]
 
   import ctx._
 
-  implicit val decodeCustomString = MappedEncoding[String, CustomString](s => CustomClass.apply(s))
+  implicit val decodeCustomString = MappedEncoding[String, CustomString](s => CustomString.apply(s))
 
   // fails because `LowPriorityImplicits.materializeDecoder` can not provide decoder for `CustomClass` `AnyVal` with private property
   // and `EncodingDsl.mappedDecoder` with `decodeCustomString` is not used at all

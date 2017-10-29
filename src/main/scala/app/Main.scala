250e4ae0f73700d86f1ad7d078aeeee75ba158e7
Michael Ledin <m.ledin@appheads.ru>
2017-10-29 23:34:45 +0300

    Code.

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,4 +1,23 @@
 package app
+import io.getquill._
+
+import scala.concurrent.{ ExecutionContext, Future }
 
 object Main extends App {
+  case class Person(name: String)
+
+  implicit val ctx = new PostgresAsyncContext(SnakeCase, "ctx")
+
+  import ctx._
+
+  def test(implicit ec: ExecutionContext): Future[Boolean] = {
+    val q = quote {
+      query[Person].nonEmpty
+    }
+    ctx.run(q)
+  }
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  test
 }
